70fe10691f85bbaa8384f016ddf877cf02e4acf0
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-03-16 16:57:58 +0700

    Staged Preview of Java tutorials

---
+++ java/MyFirstProjInJAVA/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
