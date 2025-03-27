04c28e7129ae2f5125aa14fb83c7f14b844a4230
h-sutiwas <s.jitsopak@gmail.com>
2025-03-27 23:26:02 +0700

    Basic Arithmetic in Java

---
+++ java/MyFirstProjInJAVA/src/arithmeticOperator.java
@@ -0,0 +1,49 @@
+public class arithmeticOperator {
+    public static void main(String[] args) {
+
+        //Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        z = x * y;
+        //z = x / y;
+        //z = x % 3;
+
+        System.out.println(z);
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        //x %= y;
+
+        System.out.println(x);
+
+        // Increment and Decrement Operators
+
+        int i = 1;
+
+        i++;
+        i--;
+
+        System.out.println(i);
+
+        // ORDER OF OPERATIONS [P-E-M-D-A-S]
+
+        // Parenthesis
+        // Exponential
+        // Multiplication
+        // Division
+        // Addition
+        // Subtraction
+
+        double result = 3 + 4 * 7 - 5 / 2.0;
+
+        System.out.println(result);
+
+    }
+}
