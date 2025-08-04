27c83c1652e2edcc89beb0f9c02c5c1a040e8e86
h-sutiwas <s.jitsopak@gmail.com>
2025-08-04 17:03:37 +0700

    Setup all the function and loop necessary for this. - Add time tracking - Add the number of question constraint

--- python/Medium Projects/Time Math Challenge/timeMathChallenge.py
+++ python/Medium Projects/Time Math Challenge/timeMathChallenge.py
@@ -1,8 +1,11 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -11,7 +14,23 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
+
+wrong = 0
+input("Press enter to start!")
+print("---------------------")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input(f"Problem #{str(i + 1)} : {expr} = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
