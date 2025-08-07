3b8fbfb9f9c3c7422e151053c198ba5b5b6d4f82
h-sutiwas <s.jitsopak@gmail.com>
2025-08-07 18:26:24 +0700

    Apply simple Scanner - User-Input to work around the compound interest calculator. - create a calculated variable and show the by println

--- java/MyFirstProjInJAVA/src/CompoundInterestCalculator.java
+++ java/MyFirstProjInJAVA/src/CompoundInterestCalculator.java
@@ -6,7 +6,27 @@ public class CompoundInterestCalculator {
 
         Scanner scanner = new Scanner(System.in);
 
+        double principle;
+        double rate;
+        int timesCompound;
+        int years;
+        double amount;
 
+        System.out.print("Enter the principal amount: ");
+        principle = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the number of time compounded per year: ");
+        timesCompound = scanner.nextInt();
+
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextInt();
+
+        amount = principle * Math.pow(1 + rate / timesCompound, timesCompound * years);
+
+        System.out.printf("The amount after %d years is: $%.2f", years, amount);
 
         scanner.close();
     }
