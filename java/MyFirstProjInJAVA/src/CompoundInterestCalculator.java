d02db2e1fadec2c97ed4f2ccd4c20bd284e117d1
h-sutiwas <s.jitsopak@gmail.com>
2025-08-07 18:17:44 +0700

    Create a Compound Interest Calculator with Java in most simple algorithm. - import Scanner

---
+++ java/MyFirstProjInJAVA/src/CompoundInterestCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CompoundInterestCalculator {
+    public static void main(String[] args) {
+        // Compound Interest Calculator
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+    }
+}
