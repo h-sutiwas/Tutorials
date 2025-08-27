ad91d64116d5335aee8b1d043d0592ce9148fddc
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-08-27 14:51:00 +0700

    Define another function - use this function for getting number of betting lines

--- python/Medium Projects/Slot Machine/slotMachine.py
+++ python/Medium Projects/Slot Machine/slotMachine.py
@@ -1,3 +1,5 @@
+import logging
+
 MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 100
@@ -16,7 +18,25 @@ def deposit():
         
     return amount
 
+def get_number_of_lines():
+    while True:
+        lines = input(f"How lines would you want to bet on? (1 - {MAX_LINES})")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Number of betting lines must be more than 0....")
+        else:
+            print("Please enter a number!!!")
+        
+    return lines
+
 def main():
     amount = deposit()
+    lines = get_number_of_lines()
+    
+    
+    # print(f"{amount}, {lines}")
     
 main()
