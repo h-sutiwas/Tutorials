a04a20048b208d9894e5e422409830beb3006440
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-09-02 14:25:33 +0700

    Add another function for starting the screen

--- python/Medium Projects/WPM Typing Test/WPM_Typing_Test.py
+++ python/Medium Projects/WPM Typing Test/WPM_Typing_Test.py
@@ -1,13 +1,21 @@
 import curses
 from curses import wrapper
 
-def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_WHITE)
+def startscreen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello World!", curses.color_pair(1))
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to continue...")
     stdscr.refresh()
     stdscr.getkey()
+
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
+    startscreen(stdscr)
 
     
 wrapper(main)
