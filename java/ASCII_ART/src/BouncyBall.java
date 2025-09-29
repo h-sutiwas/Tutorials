f06ae094ec84f82ed04e2c236d39d9f063fdf33b
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-09-29 15:31:33 +0700

    Update unversioned files - Create a simple BouncyBall ASCII Loop

---
+++ java/ASCII_ART/src/BouncyBall.java
@@ -0,0 +1,87 @@
+
+public class BouncyBall {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while(true) {
+            System.out.println(" O ");
+            System.out.println("   ");
+            System.out.println("___");
+
+            Thread.sleep(100);
+
+            addSpace();
+
+            System.out.println(" | ");
+            System.out.println(" O ");
+            System.out.println("___");
+
+            Thread.sleep(100);
+
+            addSpace();
+
+            System.out.println("   ");
+            System.out.println("   ");
+            System.out.println("_O_");
+
+            Thread.sleep(100);
+
+            addSpace();
+
+            System.out.println("   ");
+            System.out.println("   ");
+            System.out.println("_<>_");
+
+            Thread.sleep(100);
+
+            addSpace();
+
+            System.out.println("   ");
+            System.out.println("   ");
+            System.out.println("_O_");
+
+            Thread.sleep(100);
+
+            addSpace();
+
+            System.out.println(" O ");
+            System.out.println("   ");
+            System.out.println("_|_");
+
+            Thread.sleep(100);
+
+            addSpace();
+
+        }
+    }
+
+    public static void addSpace(){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+}
