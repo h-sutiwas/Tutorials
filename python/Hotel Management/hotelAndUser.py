d787733bb388efea8a0936da8c0465c7044729fd
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-03 21:34:46 +0700

    Adjust the functions and classmethods

--- python/Hotel Management/hotelAndUser.py
+++ python/Hotel Management/hotelAndUser.py
@@ -25,19 +25,21 @@ class Hotel:
         cls.sortParam='roomAvl'
 
     def __repr__( self ) -> str:
-        return f"PRHOTELS DATA:\nHotelName:{self.name}\tRoom Available:{self.roomAvl}\tLocation:{self.location}\tRating:{self.rating}\tPrice Per Room:{self.pricePr}"
+        return f"{self.name}\t{self.roomAvl}\t{self.location}\t{self.rating}\t{self.pricePr}"
     
 class User:
     def __init__( self ) -> None:
         self.uname = ''
-        self.uid = ''
-        self.cost = ''
+        self.uid = 0
+        self.cost = 0
 
     def __repr__( self ) -> str:
-        return f"UserName:{self.uname}\tUserId:{self.uid}\tBooking Cost:{self.cost}"
+        return f"{self.uname}\t{self.uid}\t{self.cost}"
     
 
 def PrintHotelData( hotels ):
+    print("PRINT HOTELS DATA")
+    print("HotelName\tRoom Available\tLocation\tRating\tPrice Per Room")
     for h in hotels:
         print(h)
 
@@ -53,7 +55,7 @@ def SortHotelByName( hotels ):
 
 
 def SortHotelByRating( hotels ):
-    print("SORT BY RATING:")
+    print("SORT BY A RATING:")
 
     Hotel.sortByRate()
     hotels.sort()
@@ -63,7 +65,7 @@ def SortHotelByRating( hotels ):
 
 
 def PrintHotelByCity( location, hotels ):
-    print(f"HOTELS FOR {location} LOCATION ARE:")
+    print("HOTELS FOR {} LOCATION ARE:".format(location))
     hotelsByLoc = [h for h in hotels if h.location == location]
 
     PrintHotelData( hotelsByLoc )
@@ -81,14 +83,17 @@ def SortHotelByRoomAvailable( hotels ):
 
 
 def PrintUserData( userName, userId, bookingCost, hotels ):
+    print("PRINT USER BOOKING DATA:")
+    print("UserName\tUserId\tBooking Cost\tHotelName")
+
     users = []
 
     for i in range(3):
         u=User()
-        u.uname = userName
-        u.uid = userId
-        u.cost = bookingCost
+        u.uname = userName[i]
+        u.uid = userId[i]
+        u.cost = bookingCost[i]
         users.append(u)
     
     for i in range(len(users)):
-        print(users[i], f"\tHotel Name:{hotels}")
+        print(users[i], "\t", hotels[i].name)
