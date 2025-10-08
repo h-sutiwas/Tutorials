ab58b4901ea6691c94aedca4766eedf742046789
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-08 19:01:17 +0700

    Create delete methods and prepare for a recursive delete

--- python/DSA-Mastery-Suite/common/data_structures/trie.py
+++ python/DSA-Mastery-Suite/common/data_structures/trie.py
@@ -23,14 +23,28 @@ class Trie:
             curr = curr.children[char]
         return curr.isEndOfWord
 
-    def delete(self, word: str):
-        pass
-
     def has_prefix(self, prefix: str):
-        pass
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return True
+    
+    def delete(self, word: str):
+        return self._delete(self.root, word, 0)
 
     def starts_with(self, prefix: str):
         pass
     
     def list_words(self):
         pass
+
+    def _delete(self, curr, word, index):
+        if index == len(word):
+            if not curr.isEndOfWord:
+                return False
+            
+            curr.isEndOfWord = False
+
+            return len(curr.children) == 
