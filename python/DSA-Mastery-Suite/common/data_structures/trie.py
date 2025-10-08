2b03639e96db0bc76408178e468a641eeab1e708
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-08 18:29:35 +0700

    Create a Trie method for search and fix the search method for this Trie Class

--- python/DSA-Mastery-Suite/common/data_structures/trie.py
+++ python/DSA-Mastery-Suite/common/data_structures/trie.py
@@ -1,7 +1,36 @@
 class TrieNode:
-    def __init__( self ):
-        self.children = [None]*26
+    def __init__(self):
+        self.children = dict()
         self.isEndOfWord = False
 
-    def insert( s : str ):
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str ):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.isEndOfWord = True
+
+    def search(self, word: str):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return curr.isEndOfWord
+
+    def delete(self, word: str):
+        pass
+
+    def has_prefix(self, prefix: str):
+        pass
+
+    def starts_with(self, prefix: str):
+        pass
+    
+    def list_words(self):
         pass
