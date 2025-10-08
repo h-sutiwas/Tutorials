cce38fa86e5d068e1737df271cffa45eac65ba56
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-08 19:01:43 +0700

    Update trie.py - Create a recursive _delete method - Reformatting the entire code

--- python/DSA-Mastery-Suite/common/data_structures/trie.py
+++ python/DSA-Mastery-Suite/common/data_structures/trie.py
@@ -9,26 +9,32 @@ class Trie:
 
     def insert(self, word: str ):
         curr = self.root
+
         for char in word:
             if char not in curr.children:
                 curr.children[char] = TrieNode()
             curr = curr.children[char]
+
         curr.isEndOfWord = True
 
     def search(self, word: str):
         curr = self.root
+
         for char in word:
             if char not in curr.children:
                 return False
             curr = curr.children[char]
+
         return curr.isEndOfWord
 
     def has_prefix(self, prefix: str):
         curr = self.root
+
         for char in prefix:
             if char not in curr.children:
                 return False
             curr = curr.children[char]
+            
         return True
     
     def delete(self, word: str):
@@ -47,4 +53,18 @@ class Trie:
             
             curr.isEndOfWord = False
 
-            return len(curr.children) == 
+            return len(curr.children) == 0
+        
+        char = word[index]
+        node = curr.children.get(char)
+
+        if node is None:
+            return False
+        
+        del_curr = self._delete(node, word, index + 1)
+
+        if del_curr:
+            del curr.children[char]
+            return len(curr.children) == 0 and not curr.isEndOfWord
+        
+        return 
